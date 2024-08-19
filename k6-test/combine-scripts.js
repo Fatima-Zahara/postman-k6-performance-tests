141c3549b3f0d8d00590d532d2ab5bae3c82d332
Zahara <fatima.zahara@appnetwise.com>
2024-08-19 15:40:41 +0530

    test

--- k6-test/combine-scripts.js
+++ k6-test/combine-scripts.js
@@ -1,22 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const templateFile = path.join(__dirname, 'k6-script-template.js');
-const outputFile = path.join(__dirname, 'combined-k6-script.js');
+const templatePath = path.join(__dirname, 'k6-script-template.js');
+const scriptPath = path.join(__dirname, 'UsersAPI-script.js');
+const combinedScriptPath = path.join(__dirname, 'combined-k6-script.js');
 
-let combinedScript = '';
+const templateContent = fs.readFileSync(templatePath, 'utf8');
+const scriptContent = fs.readFileSync(scriptPath, 'utf8');
 
-// Combine all generated k6 scripts
-fs.readdirSync(__dirname).forEach(file => {
-  if (file.endsWith('-script.js') && file !== 'combined-k6-script.js') {
-    const scriptContent = fs.readFileSync(path.join(__dirname, file), 'utf8');
-    combinedScript += scriptContent + '\n';
-  }
-});
-
-// Append the template content with handleSummary function
-const templateContent = fs.readFileSync(templateFile, 'utf8');
-combinedScript += templateContent;
-
-// Write the combined script to the output file
-fs.writeFileSync(outputFile, combinedScript);
+fs.writeFileSync(combinedScriptPath, `${templateContent}\n${scriptContent}`);
