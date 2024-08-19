fca476bb2bca98334e2e8751a3628ae5d1b13bcc
Zahara <fatima.zahara@appnetwise.com>
2024-08-19 11:23:54 +0530

    updated the repo structure

---
+++ k6-test/combine-scripts.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const templatePath = path.resolve(__dirname, 'k6-test/k6-script-template.js');
+const postmanScriptPath = path.resolve(__dirname, 'k6-test/k6-script.js');
+const outputPath = path.resolve(__dirname, 'k6-test/combined-k6-script.js');
+
+
+// Read the contents of the template and the generated script
+const template = fs.readFileSync(templatePath, 'utf8');
+const postmanScript = fs.readFileSync(postmanScriptPath, 'utf8');
+
+// Combine the scripts
+const combinedScript = `${postmanScript}\n\n${template}`;
+
+// Write the combined script to a new file
+fs.writeFileSync(outputPath, combinedScript, 'utf8');
+
+console.log(`Combined script written to ${outputPath}`);
