bfe09ded83bd0d58a524e0b33c302927df1e1b8c
Zahara <fatima.zahara@appnetwise.com>
2024-08-19 15:18:32 +0530

    updated the flow

--- k6-test/combine-scripts.js
+++ k6-test/combine-scripts.js
@@ -1,36 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-// Paths to your files
-const templatePath = path.resolve(__dirname, 'k6-script-template.js');
-const postmanScriptPath = path.resolve(__dirname, 'UsersAPI-script.js');
-const outputPath = path.resolve(__dirname, 'combined-k6-script.js');
+const templateFile = path.join(__dirname, 'k6-script-template.js');
+const outputFile = path.join(__dirname, 'combined-k6-script.js');
 
-// // Get the list of generated k6 script files
-// const k6ScriptFiles = fs.readdirSync(__dirname)
-//   .filter(file => file.endsWith('-script.js')); 
-
-// if (k6ScriptFiles.length === 0) {
-//   console.error('Error: No k6 script files found');
-//   process.exit(1);
-// }
-
-// Assuming you want to combine all the script files
 let combinedScript = '';
 
-k6ScriptFiles.forEach(file => {
-  const postmanScriptPath = path.resolve(__dirname, file);
-  const postmanScript = fs.readFileSync(postmanScriptPath, 'utf8');
-  combinedScript += `${postmanScript}\n\n`;
+// Combine all generated k6 scripts
+fs.readdirSync(__dirname).forEach(file => {
+  if (file.endsWith('-script.js') && file !== 'combined-k6-script.js') {
+    const scriptContent = fs.readFileSync(path.join(__dirname, file), 'utf8');
+    combinedScript += scriptContent + '\n';
+  }
 });
 
-// Read the template
-const template = fs.readFileSync(templatePath, 'utf8');
+// Append the template content with handleSummary function
+const templateContent = fs.readFileSync(templateFile, 'utf8');
+combinedScript += templateContent;
 
-// Combine the scripts with the template
-combinedScript += template;
-
-// Write the combined script to a new file
-fs.writeFileSync(outputPath, combinedScript, 'utf8');
-
-console.log(`Combined script written to ${outputPath}`);
+// Write the combined script to the output file
+fs.writeFileSync(outputFile, combinedScript);
