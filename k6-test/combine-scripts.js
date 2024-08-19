1169fa482443a53b445fed76cd22b5814e662c53
Zahara <fatima.zahara@appnetwise.com>
2024-08-19 15:08:02 +0530

    test

--- k6-test/combine-scripts.js
+++ k6-test/combine-scripts.js
@@ -3,16 +3,17 @@ const path = require('path');
 
 // Paths to your files
 const templatePath = path.resolve(__dirname, 'k6-script-template.js');
+const postmanScriptPath = path.resolve(__dirname, 'UsersAPI-script.js');
 const outputPath = path.resolve(__dirname, 'combined-k6-script.js');
 
-// Get the list of generated k6 script files
-const k6ScriptFiles = fs.readdirSync(__dirname)
-  .filter(file => file.endsWith('-script.js')); // Adjust the pattern if needed
+// // Get the list of generated k6 script files
+// const k6ScriptFiles = fs.readdirSync(__dirname)
+//   .filter(file => file.endsWith('-script.js')); 
 
-if (k6ScriptFiles.length === 0) {
-  console.error('Error: No k6 script files found');
-  process.exit(1);
-}
+// if (k6ScriptFiles.length === 0) {
+//   console.error('Error: No k6 script files found');
+//   process.exit(1);
+// }
 
 // Assuming you want to combine all the script files
 let combinedScript = '';
