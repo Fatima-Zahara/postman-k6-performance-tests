3c74fbf8d6823c8616b42c93480640667322726a
Zahara <fatima.zahara@appnetwise.com>
2024-08-20 13:09:16 +0530

    fix k6 with reports

--- k6-test/combine-scripts.js
+++ k6-test/combine-scripts.js
@@ -5,6 +5,10 @@ const templatePath = path.join(__dirname, 'k6-script-template.js');
 const scriptPath = path.join(__dirname, 'k6-script.js');
 const combinedScriptPath = path.join(__dirname, 'combined-k6-script.js');
 
+if (!fs.existsSync(scriptPath)) {
+  throw new Error(`File not found: ${scriptPath}`);
+}
+
 const templateContent = fs.readFileSync(templatePath, 'utf8');
 const scriptContent = fs.readFileSync(scriptPath, 'utf8');
 
